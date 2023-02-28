d60f2dc6f3d8788603dfee15ff77383950c8797e
Farzana-BL <farzana.shaikh@bridgelabz.com>
2023-02-28 21:18:56 +0530

    [Farzana] Add : This is a bash script to take user input

---
+++ SampleShellProg/2_UserInput.sh
@@ -0,0 +1,10 @@
+# 2. A script that prompts the user to enter their name and outputs a greeting.
+
+
+#!/bin/bash
+
+#read -p  "Enter your name: " name
+
+echo "Please enter your name"
+read name
+echo "Hello, $name!"
